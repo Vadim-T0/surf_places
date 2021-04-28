93d8f52f335a0e541599250b7a5bb2d81e7e44f0
Vadim-T0 <tiunov.v@onrela.ru>
2021-04-28 18:57:29 +0700

    4.2.1 Создание экрана.

---
+++ lib/ui/screen/sight_list_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// Экран приложения.
+class SightListScreen extends StatefulWidget {
+  @override
+  _SightListScreenState createState() => _SightListScreenState();
+}
+
+class _SightListScreenState extends State<SightListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
